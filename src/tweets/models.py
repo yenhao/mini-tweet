eca6fafd8d635c6455cf673fea00a67cd8f117d1
yenhao0218 <yenhao0218@gmail.com>
2017-03-25 17:05:46 +0800

    create tweet-related admin view html

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -1,5 +1,14 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Tweet(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default = 1)
+    content = models.CharField(max_length=140)
+    timestamp = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now = True)
+
+    # Kind of title in the admin pages
+    def __str__(self):
+        return self.content
