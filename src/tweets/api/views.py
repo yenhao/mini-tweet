875ef8a515491e06453269a029357fa950a39c47
yenhao0218 <yenhao0218@gmail.com>
2017-04-12 00:44:30 +0800

    ajax query api

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import generics
 
 from tweets.models import Tweet
@@ -8,6 +9,18 @@ class TweetListAPIView(generics.ListAPIView):
 
     # queryset = Tweet.objects.all()
 
-    def get_queryset(self):
-        # return self.queryset
-        return Tweet.objects.all()
+    # def get_queryset(self): # basic
+    #     # return self.queryset
+    #     return Tweet.objects.all()
+
+    def get_queryset(self, *args, **kwargs):
+        qs = Tweet.objects.all()
+        print(self.request.GET.get('q'))
+        query = self.request.GET.get('q')
+        if query is not None:
+            # qs = qs.filter(content__icontains = query) # basic
+            qs = qs.filter(
+                    Q(content__icontains = query) |
+                    Q(user__username__icontains = query)
+                )
+        return qs
