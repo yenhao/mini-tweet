9c6e25327db0a46677460fd2229b04cb2c15643a
yenhao0218 <yenhao0218@gmail.com>
2017-07-11 22:33:05 +0800

    personal page setting

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name = 'profile')
+    following = models.ManyToManyField(settings.AUTH_USER_MODEL, blank = True, related_name = 'followed_by')
+
+    def __str__(self):
+        return str(self.following.all().count())
